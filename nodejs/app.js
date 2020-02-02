f5cc43f49aff4f8fb496116d485bbbca324b4460
smoll <mollah@gmail.com>
2020-02-02 01:21:41 -0500

    init node boilerplate

---
+++ nodejs/app.js
@@ -0,0 +1,13 @@
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const main = async () => {
+  console.log(`started at ${Date.now()}`)
+  await wait(100) // ms
+  console.log(`ended at ${Date.now()}`)
+}
+
+if (require.main === module) {
+  main()
+}
+
+module.exports = main
