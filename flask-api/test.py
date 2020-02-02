ed46f893bf740a3e2a82cc636a301313662d9b43
smoll <mollah@gmail.com>
2020-02-02 02:05:00 -0500

    language agnostic wait

--- flask-api/test.py
+++ flask-api/test.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import unittest
-from test_utils import wait_for_server
 
 API_HOST = os.getenv('API_HOST')
 API_PORT = os.getenv('API_PORT')
@@ -15,5 +14,4 @@ class Test(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    wait_for_server(API_HOST, API_PORT)
     unittest.main()
