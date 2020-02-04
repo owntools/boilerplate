e32f66d63817e2c50b85b0167e93a0ed9cedaabb
Shujon Mollah <mollah@gmail.com>
2020-02-04 01:07:16 -0500

    same for express

--- express-api/test.js
+++ express-api/test.js
@@ -1,14 +1,12 @@
-import request from 'supertest'
+import request from "supertest";
+import app from "./app";
 
-const {API_URL} = process.env
-if (!API_URL) throw new Error(`invalid API_URL: ${API_URL}. check env vars.`)
-
-describe('GET /', () => {
-  it('responds with json', done => {
-    request(API_URL)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  })
-})
+describe("GET /", () => {
+  it("responds with json", done => {
+    request(app)
+      .get("/")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
